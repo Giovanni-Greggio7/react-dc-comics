8ad4bac2ccbbc3e9335e7622d07068cae4f7e174
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-02-21 11:34:27 +0100

    Add: Data folder in src

---
+++ src/data/links.js
@@ -0,0 +1,18 @@
+const links = 
+    [
+        { id: 1, text: 'CHARACTERS', url: '#', current: true },
+        { id: 2, text: 'COMICS', url: '#', current: false },
+        { id: 3, text: 'MOVIES', url: '#', current: false },
+        { id: 4, text: 'TV', url: '#', current: false },
+        { id: 5, text: 'GAMES', url: '#', current: true },
+        { id: 6, text: 'COLLECTIBLES', url: '#', current: true },
+        { id: 7, text: 'VIDEOS', url: '#', current: true },
+        { id: 8, text: 'FANS', url: '#', current: false },
+        { id: 9, text: 'NEWS', url: '#', current: false },
+        { id: 10, text: 'SHOP', url: '#', current: true }
+      ];
+
+
+
+  export default links
+
